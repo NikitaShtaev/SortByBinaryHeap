bd247fe6b56eef6285693ae65cd456326924aa64
Nikita Shtaev <nikitashtaev@mail.ru>
2020-06-04 22:15:21 +0300

    Add project files.

---
+++ BinaryHeapSort.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortByBinaryHeap
+{
+    public class BinaryHeapSort<T>
+        where T: IComparable
+    {
+        /// <summary>
+        /// Coleection of elements.
+        /// </summary>
+        public List<T> Items = new List<T>();
+        /// <summary>
+        /// Quantity of elements.
+        /// </summary>
+        public int Count => Items.Count;
+        public BinaryHeapSort(){ }
+        public BinaryHeapSort(IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                Add(item);
+            }
+        }
+        /// <summary>
+        /// Adding element to collection.
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(T item)
+        {
+            Items.Add(item);
+            BalanceHeap();
+        }
+        /// <summary>
+        /// Sort of collection and return it as List.
+        /// </summary>
+        /// <returns></returns>
+        public List<T> Sort()
+        {
+            var lst = new List<T>();
+            while (Items.Count > 0)
+            {
+                lst.Add(GetMax());
+            }
+            return lst;
+        }
+        /// <summary>
+        /// Returns first item from collection(max). Copy collection and going throw it by deleting first item. 
+        /// </summary>
+        /// <returns></returns>
+        private T GetMax()
+        {
+            var newItems = Items;
+            var result = newItems[0];
+            newItems[0] = newItems[Count - 1];
+            newItems.RemoveAt(Count - 1);
+            Sort(0);
+            return result;
+        }
+        /// <summary>
+        /// Sort program for collection based on parant and current indexes.
+        /// </summary>
+        /// <param name="currentIndex"></param>
+        private void Sort(int currentIndex)
+        {
+            int leftIndex, rightIndex;
+            int maxIndex = currentIndex;
+            while (currentIndex < Count)
+            {
+                leftIndex = 2 * currentIndex + 1;
+                rightIndex = 2 * currentIndex + 2;
+                if (leftIndex < Count && Items[maxIndex].CompareTo(Items[leftIndex]) == -1)
+                {
+                    maxIndex = leftIndex;
+                }
+                if (rightIndex < Count && Items[maxIndex].CompareTo(Items[rightIndex]) == -1)
+                {
+                    maxIndex = rightIndex;
+                }
+                if (maxIndex == currentIndex)
+                {
+                    break;
+                }
+                SwapMethod(currentIndex, maxIndex);
+                currentIndex = maxIndex;
+            }
+        }
+        /// <summary>
+        /// Balancing for collection for adding element.
+        /// </summary>
+        private void BalanceHeap()
+        {
+            int currentIndex = Count - 1;
+            int parantIndex = GetParantIndex(currentIndex);
+            while (currentIndex > 0 && Items[parantIndex].CompareTo(Items[currentIndex]) == -1)
+            {
+                SwapMethod(currentIndex, parantIndex);
+                currentIndex = parantIndex;
+                parantIndex = GetParantIndex(currentIndex);
+            }
+        }
+        /// <summary>
+        /// Swap elements of collection.
+        /// </summary>
+        /// <param name="currentIndex"></param>
+        /// <param name="parantIndex"></param>
+        private void SwapMethod(int currentIndex, int parantIndex)
+        {
+            var temp = Items[currentIndex];
+            Items[currentIndex] = Items[parantIndex];
+            Items[parantIndex] = temp;
+        }
+        /// <summary>
+        /// Returns parant index for current element by it's index.
+        /// </summary>
+        /// <param name="currentIndex"></param>
+        /// <returns></returns>
+        private int GetParantIndex(int currentIndex)
+        {
+            return (currentIndex - 1) / 2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SortByBinaryHeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lst = new List<int>();
+            var rnd = new Random();
+            var toBeSortCollection2 = new BinaryHeapSort<int>();
+            var timer = new Stopwatch();
+
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            timer.Start();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                lst.Add(rnd.Next(-1000, 1000));
+            }
+            var toBeSortCollection = new BinaryHeapSort<int>(lst);
+
+            var lst2 = toBeSortCollection.Sort();
+            //foreach (var item in lst2)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            timer.Stop();
+            Console.WriteLine($"\n{timer.Elapsed}");
+
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            timer.Reset();
+            timer.Start();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                toBeSortCollection2.Add(rnd.Next(-1000, 1000));
+            }
+
+            var lst3 = toBeSortCollection2.Sort();
+            //foreach (var item in lst3)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            timer.Stop();
+            Console.WriteLine($"\n{timer.Elapsed}");
+            
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            //timer.Reset();
+            //timer.Start();
+
+            //int temp;
+            //for (int i = 0; i < lst.Count; i++)
+            //{
+            //    for (int j = i; j < lst.Count; j++)
+            //    {
+            //        if (lst[i] < lst[j])
+            //        {
+            //            temp = lst[i];
+            //            lst[i] = lst[j];
+            //            lst[j] = temp;
+            //        }
+            //    }
+            //}
+            ////foreach (var item in lst)
+            ////{
+            ////    Console.Write(item + " ");
+            ////}
+            //timer.Stop();
+            //Console.WriteLine($"\n{timer.Elapsed}");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            Console.ReadLine();
+        }
+    }
+}
